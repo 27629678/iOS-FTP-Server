4d7d0b4f27be78a413e7a63c09858b5aac48b16a
yuxiaohua <hzyuxiaohua@corp.netease.com>
2017-03-31 14:21:08 +0800

    Initial Commit

---
+++ ftp-server-ios/ftp-server-ios.h
@@ -0,0 +1,19 @@
+//
+//  ftp-server-ios.h
+//  ftp-server-ios
+//
+//  Created by hzyuxiaohua on 2017/3/31.
+//  Copyright © 2017年 XY Network Co., Ltd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ftp-server-ios.
+FOUNDATION_EXPORT double ftp_server_iosVersionNumber;
+
+//! Project version string for ftp-server-ios.
+FOUNDATION_EXPORT const unsigned char ftp_server_iosVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ftp_server_ios/PublicHeader.h>
+
+
